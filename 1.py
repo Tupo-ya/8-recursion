941dca21ecbbe2da05968a358be621b85971c363
Tushik <mnsk_5455@mail.ru>
2023-10-28 23:19:48 +0300

    Рекурсия

--- 1.py
+++ 1.py
@@ -13,4 +13,4 @@ def find_max(arr, index=0, now_max=float('-inf')):
 
 print(find_max([1,2,3,4,1]))
 print(find_max([21,4,1]))
-print(find_max([1,2,41,4,1,1,231,5,7,8,8,8]))
+print(find_max([2,41,4,-121,1,238]))
